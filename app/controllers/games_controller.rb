fc448905b668e637bdcb7aaf2ed372a1f5aa9bbf
Sean Oughton <seanoughton@gmail.com>
2018-07-17 17:26:17 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,20 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+  Create — POST — /games
+  Show — GET — /games/:id
+  Update — PATCH — /games/:id
+  Index — GET — /games
+
 end
